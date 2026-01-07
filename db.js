38ce0bce83666c60211189c01331f3a2e9bc3344
wimagolo2806-hue <wimagolo2806@gmail.com>
2026-01-07 01:40:13 -0500

    fix: render supabase db connection

--- db.js
+++ db.js
@@ -1,13 +1,11 @@
 // db.js
-const { Pool } = require('pg');
-require('dotenv').config();
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT ? Number(process.env.PGPORT) : 5432
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
